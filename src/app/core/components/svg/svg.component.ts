4910254e15ae356cf8cac0f2903615fd6ffcb1dd
MTDEE <deekub8147@gmail.com>
2024-02-16 11:38:03 +0700

    [Updated] EXAT-28 Login

--- src/app/core/components/svg/svg.component.ts
+++ src/app/core/components/svg/svg.component.ts
@@ -6,44 +6,54 @@ import { AfterViewInit, Component, HostBinding, Input, OnInit } from '@angular/c
   styleUrl: './svg.component.scss'
 })
 export class SvgComponent implements OnInit {
-
   @HostBinding('style.-webkit-mask-image') private _path!: string;
 
-  // @HostBinding('style.width') private _width!: string;
-
   @Input() public path: string | undefined;
-  @Input() public size: 'sm' | 'md' | 'lg' | 'xl' = 'sm'; // สร้าง Input property สำหรับ size
-  @Input() public color: string = '#ffffff';
-
-  public dimension: number = 20;
-
-  constructor() {
-
-  }
+  @Input() public size: 's' | 'm' | 'l' | 'xl' = 'm';
+  @Input() public color: 'white' | 'blue' = 'white'; // Fixed typo here
 
   ngOnInit(): void {
     if (this.path) {
-      this._path = `url("${ this.path} ")`;
-      // this._width = this.getSize()+'px';
+      this._path = `url("${this.path}")`;
     }
   }
 
-  ngAfterViewInit(): void {
-  }
-
-  private getSize(): number {
-    switch (this.size) {
-      case 'sm':
-        return 15;
-      case 'md':
-        return 20;
-      case 'lg':
-        return 25;
-      case 'xl':
-        return 30;
+  getColor(): string {
+    let colorcode: string;
+    switch (this.color) {
+      case 'white':
+        colorcode = '#FFFFFF';
+        break;
+      case 'blue':
+        colorcode = '#2255CE';
+        break; // Added missing break statement
       default:
-        return 20;
+        colorcode = '#FFFFFF'; // Default to white if no match
+        break;
     }
+    return colorcode;
+  }
+
+  getSize(): string {
+    let dimension: string;
+    switch (this.size) {
+      case 's':
+        dimension = '15px';
+        break;
+      case 'm':
+        dimension = '24px';
+        break;
+      case 'l':
+        dimension = '25px';
+        break;
+      case 'xl':
+        dimension = '30px';
+        break;
+      default:
+        dimension = '20px';
+        break;
+    }
+    return dimension;
   }
 
 }
--- src/app/features/account/pages/login/login.component.ts
+++ src/app/features/account/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router'; // เพิ่ม Router มาใน import
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+    
+  public form: FormGroup = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  });
+  
+  constructor(
+    private router: Router // เพิ่ม Router ไปยัง constructor
+  ) {
+ 
+  }
 
+  onLogin(): void {
+    if (this.form.valid) {
+      const enteredUsername = this.form.value.username;
+      const enteredPassword = this.form.value.password;
+
+      // กระทำตามลอจิกการตรวจสอบข้อมูลที่นี่
+      if (enteredUsername === 'admin' && enteredPassword === 'admin') {
+        console.log('Login successful');
+        this.router.navigate(['/work-space/menu-option']); // เปลี่ยนเส้นทางไปยังหน้า 'home'
+      } else {
+        console.log('Login failed. Invalid credentials.');
+      }
+      if  (enteredUsername === 'superadmin' && enteredPassword === 'superadmin') {
+        console.log('Login successful');
+        this.router.navigate(['/work-space/menu-option']); // เปลี่ยนเส้นทางไปยังหน้า 'home'
+      } else {
+        console.log('Login failed. Invalid credentials.');
+    } 
+    }
+  }
 }
