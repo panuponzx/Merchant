0fe4ddb72627b04dc421643aaca60f1df2c879be
MTDEE <deekub8147@gmail.com>
2024-02-21 10:27:19 +0700

    [Updated] EXAT-24 menu-option&navbar

--- src/app/features/workspace/pages/workspace-with-navbar/pages/menu-option/menu-option.component.ts
+++ src/app/features/workspace/pages/workspace-with-navbar/pages/menu-option/menu-option.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserModel } from '../../../../../../core/interfaces';
+import { AuthenticationService } from '../../../../../../core/services';
 
 @Component({
   selector: 'app-menu-option',
@@ -8,9 +10,14 @@ import { Router } from '@angular/router';
 })
 export class MenuOptionComponent {
 
+  public user: UserModel | undefined;
+
   constructor(
-    private router: Router
+    private router: Router,
+    private authenticationService: AuthenticationService 
   ) {
+    this.authenticationService.user?.subscribe(x => this.user = x);
+    console.log(this.user)
     addEventListener('dblclick', () => {
       this.router.navigate(['work-space/user-info/1']);
     })
