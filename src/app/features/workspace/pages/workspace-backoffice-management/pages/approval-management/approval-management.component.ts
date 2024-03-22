3f8f850f52800974517ef66dbdc65889dabae781
MTDEE <deekub8147@gmail.com>
2024-03-22 14:32:53 +0700

    [Updated] setting-earn-campaign/manage-earning

--- src/app/features/workspace/pages/workspace-backoffice-management/pages/approval-management/approval-management.component.ts
+++ src/app/features/workspace/pages/workspace-backoffice-management/pages/approval-management/approval-management.component.ts
@@ -26,7 +26,7 @@ export class ApprovalManagementComponent {
   public activeTab: 'waiting-for-approval' | 'approval' | 'reject' | string | null;
 
   public columns: CustomColumnModel[] = [
-    { id: 'createDate', name: 'Create Date', label: 'วันที่ และ เวลา', prop: 'createDate', sortable: false, resizeable: true, width: 200, minWidth: 200, headerClass: 'text-break text-center', cellClass: 'text-break text-center', type: 'date', date: { format: 'D MMMM BBBB HH:mm:ss', locale: 'th' } },
+    { id: 'createDate', name: 'Create Date', label: 'วันที่ และ เวลา ที่สร้าง', prop: 'createDate', sortable: false, resizeable: true, width: 200, minWidth: 200, headerClass: 'text-break text-center', cellClass: 'text-break text-center', type: 'date', date: { format: 'D MMMM BBBB HH:mm:ss', locale: 'th' } },
     { id: 'userName', name: 'User Name', label: 'ชื่อผู้ใช้งาน', prop: 'userName', sortable: false, resizeable: true, width: 200, minWidth: 200, headerClass: 'text-break text-center', cellClass: 'text-break text-center', type: 'text' },
     { id: 'status', name: 'Status', label: 'สถานะ', prop: 'status', sortable: false, resizeable: true, width: 130, minWidth: 130, headerClass: 'text-break text-center', cellClass: 'text-break text-center', type: 'text' },
     { id: 'nameOfEmployee ', name: 'Name of Employee', label: 'ชื่อพนักงานทำรายการ', prop: 'nameOfEmployee', sortable: false, resizeable: true, width: 150, minWidth: 150, headerClass: 'text-break text-center', cellClass: 'text-center text-break', type: 'text' },
---
+++ src/app/features/workspace/pages/workspace-backoffice-management/pages/setting-earn-campaign/index.ts
@@ -0,0 +1,2 @@
+export * from './tabs/manage-earning/manage-earning.component'
+export * from './tabs/manage-redeem/manage-redeem.component'
