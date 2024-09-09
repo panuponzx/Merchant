7f86f91f7b234c19dcd09507082960666b3fd441
psk_dream <pongsakorn_s@brainergy.digital>
2024-09-09 16:52:44 +0700

    fix: set env

--- set_env.sh
+++ set_env.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export RUNNING=$RANDOM
+echo $RUNNING
 envsubst < /usr/share/nginx/html/assets/env.template.js > /usr/share/nginx/html/assets/env.$RUNNING.js
 sed -i "s/env.min.js/env.$RUNNING.js/g" /usr/share/nginx/html/index.html
 exec nginx -g 'daemon off;'
